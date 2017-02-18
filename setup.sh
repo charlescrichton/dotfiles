e38d9b978da4953862cbc0d60510d737df1a9fa2
Charles Crichton <charles.crichton@ndm.ox.ac.uk>
2017-02-18 17:32:35 +0000

    Very simple linking of dot files

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+
+ln -s ~/Repo/config/dotfiles/zshrc ~/.zshrc 
+ln -s ~/Repo/config/dotfiles/zshenv ~/.zshenv
+ln -s ~/Repo/config/dotfiles/gitconfig ~/.gitconfig
+ln -s ~/Repo/config/dotfiles/profile ~/.profile
+
+
